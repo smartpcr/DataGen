a6a31fe981ef9caca350f6ff1869a2cf02f9ee7f
xiaodong <li.xiaodong@biosoftworks.com>
2015-09-16 17:34:52 -0400

    updated oet.feature to use generic column reader

---
+++ src/main/java/org/finra/test/datagen/util/PropertyReader.java
@@ -0,0 +1,7 @@
+package org.finra.test.datagen.util;
+
+/**
+ * Created on 9/16/2015.
+ */
+public class PropertyReader {
+}
---
+++ src/test/java/org/finra/test/datagen/model/ExcelRecord.java
@@ -0,0 +1,7 @@
+package org.finra.test.datagen.model;
+
+/**
+ * Created on 9/16/2015.
+ */
+public class ExcelRecord {
+}
